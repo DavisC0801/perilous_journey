c9ea3ffd73836414450c45eb11f0edd7630feb78
davisc0801 <davisc1014@gmail.com>
2019-05-17 09:31:35 -0600

    Add tests for basic node functionality

--- test/node_test.rb
+++ test/node_test.rb
@@ -2,5 +2,19 @@ require "./test/test_helper.rb"
 require "./lib/node.rb"
 
 class Nodetest < Minitest::Test
+  def setup
+    @node = Node.new("Davis")
+  end
 
+  def test_it_exists
+    assert_instance_of Node, @node
+  end
+
+  def test_it_has_a_saved_surname
+    assert_equal @node.surname , "Davis"
+  end
+
+  def test_it_has_a_nil_next_node_by_default
+    assert_nil @node.next_node
+  end
 end
