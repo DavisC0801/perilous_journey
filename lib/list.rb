a0cbab5dd241c86e4f11fb7d87a72d065d0aa189
davisc0801 <davisc1014@gmail.com>
2019-06-11 15:40:29 -0600

    Add include functionality

--- lib/list.rb
+++ lib/list.rb
@@ -9,8 +9,8 @@ class List
   end
 
   def append(surname)
-    tail(@head).add_node(Node.new(surname)) if @head != nil
-    @head = Node.new(surname) if @head == nil
+    tail(@head).add_node(Node.new(surname)) if !@head.nil?
+    @head = Node.new(surname) if @head.nil?
   end
 
   def count
@@ -29,7 +29,7 @@ class List
     @head = new_head
   end
 
-  def insert(index, surname)
+  def insert(index = 0, surname)
     new_node = Node.new(surname)
     @count = 0
     insert_node = find_node_by_index(index - 1, @head)
@@ -38,38 +38,38 @@ class List
   end
 
   def tail(node)
-    return node if node.next_node == nil
+    return node if node.next_node.nil?
     tail(node.next_node)
   end
 
-  def find_node_by_index(index, node)
-    return node if @count == index || node.next_node == nil
+  def find_node_by_index(index = 0, node = @head)
+    return node if @count == index || node.next_node.nil?
     @count += 1
     find_node_by_index(index, node.next_node)
   end
 
   def count_list(node)
-    return @count if node == nil
+    return @count if node.nil?
     @count += 1
     count_list(node.next_node)
   end
 
   def string_list(node)
-    return @string if node == nil
+    return @string if node.nil?
     @string = "The #{node.surname} family" if node == @head
     @string += ", followed by the #{node.surname} family" if node != @head
     string_list(node.next_node)
   end
 
   def string_list_limited(node, limit)
-    return @string if @count >= limit || node == nil
-    @string += ", followed by the #{node.surname} family" if @string != nil
-    @string = "The #{node.surname} family" if @string == nil
+    return @string if @count >= limit || node.nil?
+    @string += ", followed by the #{node.surname} family" if !@string.nil?
+    @string = "The #{node.surname} family" if @string.nil?
     @count += 1
     string_list_limited(node.next_node, limit)
   end
 
-  def find(index, count)
+  def find(index = 0, count = 1)
     @count = 0
     start_node = find_node_by_index(index, @head)
     @string = nil
@@ -77,4 +77,9 @@ class List
     string_list_limited(start_node, count)
   end
 
+  def includes?(search_value, node = @head)
+    return false if node.nil?
+    return true if node.surname == search_value
+    includes?(search_value, node.next_node)
+  end
 end
